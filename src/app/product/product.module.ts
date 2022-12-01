100147f8062af7dd7586e403ce1b7d806b6008bf
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-12-01 22:33:26 +0530

    Product Folder

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { ProductComponent } from './product.component';
-
 
 
 @NgModule({
   declarations: [
+    ProductComponent
+  ],
+  exports: [
     ProductComponent
   ],
   imports: [
