72b3b255c54504aed22e9a010fdbf25d1ec82416
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-12-08 21:09:56 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,16 @@ export class AppComponent {
   title = 'eShop Application';
 
   // Robbie, Why do we need this? Also, why it is always empty?
+  // Thanks Robbie, This is expected. And it is the a safety feature.
   processEnv = globalThis.process?.env;
-
+  // processEnv = "Robbie, Why do we need this? Also, why it is always empty?";
   angularEnv = environment;
-  
+
+  constructor() {
+    console.log(JSON.stringify(this.processEnv), JSON.stringify(globalThis.process));
+
+    console.log(JSON.stringify(Object.getOwnPropertyNames(this)));
+  }
+
+
 }
